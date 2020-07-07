3f93bf45a544bfbf68b49d2ba440c493ace58642
ayfokin <ayfokin@mail.ru>
2020-07-07 14:37:08 +0500

    fixed tests

--- src/test/java/DataParserTest.java
+++ src/test/java/DataParserTest.java
@@ -8,32 +8,98 @@ public class DataParserTest {
     private static final Logger logger = LogManager.getLogger();
 
     @Test
-    public void test_getString() {
+    public void testCity_getString() {
         Instance.initializeData();
-        logger.info("Data initialized successfully");
 
         Assert.assertEquals("Москва", DataParser.getCity("московский", Instance.cities));
+        logger.debug("testCity_getString passed");
+    }
+
+    @Test
+    public void testCity2Words_getString() {
+        Instance.initializeData();
+
         Assert.assertEquals("Великий Новгород", DataParser.getCity("великий новгород", Instance.cities));
+        logger.debug("testCity2Words_getString passed");
+    }
+
+    @Test
+    public void testCityAbbreviated_getString() {
+        Instance.initializeData();
+
         Assert.assertEquals("Санкт-Петербург", DataParser.getCity("спб", Instance.cities));
+        logger.debug("testCityAbbreviated_getString passed");
+    }
+
+    @Test
+    public void testCityEmpty_getString() {
+        Instance.initializeData();
+
         Assert.assertNull(DataParser.getCity("", Instance.cities));
-        Assert.assertNull(DataParser.getCity("новгород", Instance.cities));
-        logger.debug("getString passed tests with cities");
+        logger.debug("testCityEmpty_getString passed");
+    }
+
+    @Test
+    public void testCurrencyEur_getString() {
+        Instance.initializeData();
 
         Assert.assertEquals("eur", DataParser.getCity("евро в европе", Instance.currencies));
-        Assert.assertEquals("jpy", DataParser.getCity("а иены", Instance.currencies));
+        logger.debug("testCurrencyEur_getString passed");
+    }
+
+    @Test
+    public void testCurrencyJpy_getString() {
+        Instance.initializeData();
+
         Assert.assertEquals("jpy", DataParser.getCity("а что насчет йен", Instance.currencies));
+        logger.debug("testCurrencyJpy_getString passed");
+    }
+
+    @Test
+    public void testCurrencyNo_getString() {
+        Instance.initializeData();
+
         Assert.assertNull(DataParser.getCity("европейцы", Instance.currencies));
+        logger.debug("testCurrencyNo_getString passed");
+    }
+
+    @Test
+    public void testCurrencyEmpty_getString() {
+        Instance.initializeData();
+
         Assert.assertNull(DataParser.getCity("", Instance.currencies));
-        logger.debug("getString passed tests with currencies");
+        logger.debug("testCurrencyNo_getString passed");
     }
 
     @Test
     public void test_getDate() {
+        Instance.initializeData();
+
         Assert.assertEquals("25.05.2019", DataParser.getDate("qwe25.05.2019rty"));
-        Assert.assertEquals("01.06.2019", DataParser.getDate("32.05.2019"));
-        Assert.assertEquals("30.11.0002", DataParser.getDate("00.00.0000"));
+        logger.debug("test_getDate passed");
+    }
+
+    @Test
+    public void testNonexistent_getDate() {
+        Instance.initializeData();
+
+        Assert.assertEquals("0", DataParser.getDate("32.05.2019"));
+        logger.debug("testNonexistent_getDate passed");
+    }
+
+    @Test
+    public void testWrongFormat_getDate() {
+        Instance.initializeData();
+
         Assert.assertEquals("0", DataParser.getDate("11/11/2011"));
+        logger.debug("testWrongFormat_getDate passed");
+    }
+
+    @Test
+    public void testFuture_getDate() {
+        Instance.initializeData();
+
         Assert.assertEquals("1", DataParser.getDate("покажи курс цб на 25.05.2045"));
-        logger.debug("getDate passed tests");
+        logger.debug("testFuture_getDate passed");
     }
 }
